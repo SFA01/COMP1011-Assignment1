812844b983473de64864bfd4ce61fa358eac20a8
SFA01 <sarafranzinoandrade@hotmail.com>
2022-06-06 19:44:05 -0400

    Getting rid of errors

--- src/main/java/ca/georgian/comp1011assignment1/HelloController.java
+++ src/main/java/ca/georgian/comp1011assignment1/HelloController.java
@@ -1,14 +1,20 @@
 package ca.georgian.comp1011assignment1;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+
+    public void OnClickedButton1(ActionEvent actionEvent) {
     }
 }
